6478ff1b62927bdb5960953e84db1da49366bdd8
peacheyg <peacheyg@susqu.edu>
2022-09-21 15:24:05 -0400

    Update Peachey_Week3_Assignment.R

--- code/Peachey_Week3_Assignment/Peachey_Week3_Assignment.R
+++ code/Peachey_Week3_Assignment/Peachey_Week3_Assignment.R
@@ -1,17 +1,26 @@
 # Approximately how many hours ahead of Sunbury was the peak flow in Lewisburg during the 2011 flood? (1 pt)
-
+# approximately 9 hours
 
 # Give one reason why information on the time between peak flow events up- and downstream could be valuable information? (3 pts)
-
+#It could be valuable especially for the downstream area to estimate when flooding events will occur most extremely. This would allow the residents of the area to be able to safely evacuate if needed or take precautionary measures.
 
 # Package scavenger hunt! (8 pts each)
 
 ## 1) Using Google and ONLY packages from GitHub or CRAN:
     # Find a package that contains at least one function specifically designed to measure genetic drift.
-    # Copy-paste into your script - and run - an example from the reference manual for a function within this package related to a measure of genetic drift. 
+pkgs <- c("plyr","reshape","ggplot2","magrittr","viridis","shiny")
+dl_pkgs <- subset(pkgs,!pkgs %in% rownames(installed.packages()))
+if(length(dl_pkgs)!=0){
+  for(i in dl_pkgs) install.packages(i)
+}
+library(shiny)
+runGitHub(username="cjbattey",repo="driftR")
+ # Copy-paste into your script - and run - an example from the reference manual for a function within this package related to a measure of genetic drift. 
         # Depending on the function, either upload a plot of the result or use print() and copy/paste the console output into your script.
-    # After running the function example, manipulate a parameter within the function to create a new result. 
+    
+# After running the function example, manipulate a parameter within the function to create a new result. 
         # Common options might be allele frequency, population size, fitness level, etc. 
+# changed population size from 100 to 1000
         # Add the results of this manipulation to your script (if in the console) or upload the new plot.
        
           # By manipulating these parameters you can see how it impacts the results.
@@ -22,7 +31,15 @@
 ## 2) Using Google and ONLY packages from GitHub or CRAN:
     # Find a package that will generate standard diversity metrics for community ecology, specifically Simpson's Diversity Index.
     # Copy-paste into your script - and run - an example from the reference manual for a function to calculate Simpson's diversity. 
-        # Depending on the example usage of the function, either upload a plot of the result or use print() and copy/paste the console output into your script.
+install.packages("devtools")
+library(devtools)
+install.packages("devtools")
+devtools::install_github("shearer/simboot")
+
+data(Bacteria)
+library(simboot)
+
+ # Depending on the example usage of the function, either upload a plot of the result or use print() and copy/paste the console output into your script.
     # After running the function example, modify your script to generate another diversity metric that is NOT part of the example. 
         # If there are two diversity metrics in the example script, neither of these will count as the modified script.
         # Hint: If the function can "only" caluclate Simpson's diversity, the inverse of Simpson's diversity is another common metric. 
