c3e9a8fe9e5180a86010f8d60fe8a3f05292da4c
Matt Wilson <wilsonmatt@susqu.edu>
2022-10-24 13:59:09 -0400

    Wk4 graded

--- code/Peachey_Week4_GeneticsAssignment.R
+++ code/Peachey_Week4_GeneticsAssignment.R
@@ -3,6 +3,7 @@
 #The data might be difficult to draw spatial inferences about genes because it doesn't show topography, and there's not enough randomly sampled data.
   # Part 2: Despite the drawbacks, give the result or interpretation that you feel most confident in (3 points), and EXPLAIN WHY (4 points).
 #The interpretation I feel most confident in is that more randomly-sampled data needs to be collected and other factors such as topography should be considered to fully understand the relationship between the genes and their geographic region.
+#This is not an interpretation, it's a rehash of Part 1.
 
 # For your scripting assignment we will use the "ge_data" data frame found in the "stability" package.
   # Install the "stability" package, load it into your R environment, and use the data() function to load the "ge_data". (2 points)
@@ -26,5 +27,7 @@ summary(mod.Gen)
   # Which model is a better fit to explain the yield response, and WHY? (6 points)
   # Hint: Does one model seem more likely to be over-fitted?
 #Environment is a better fit to explain the yield response because it has a smaller p value.
+#p-value is not the only reason, and is only one of several ways we can decide which is the best fit for the data without over-fitting.
+
 # Which environment would be your very WORST choice for generating a strong yield response? (2 points)
 #Sargodha would be the worst choice for generating a strong yield response.
